cc0ea2ceedde2c4732d2bec2b4402f95438b3e0c
faruqneo <faruq@marmeto.com>
2019-01-22 08:35:46 +0530

    trying to retrive json data from url but render is not working

--- crud.js
+++ crud.js
@@ -1,62 +1,37 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const request = require('request');
 
 //Init App
 const app = express();
-
-//Connect Database
-mongoose.connect('mongodb://localhost/curd');
-let db = mongoose.connection;
-
-//check connection
-db.once('open',function(){
-	console.log('Connected to mongodb');
-});
-
-//check connection error
-db.on('error',function(){
-	console.log(error);
-});
-
-
-//Connected with model
-let Class = require('./models/class');
 
 
 //Load View Engine
 app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
-// Body parser Middelware
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
-app.use(bodyParser.json());
+const url = "https://fcc-weather-api.glitch.me/api/current?lat=20.360451&lon=85.8246128";
 
 app.get('/',function(req, res){
-	res.render('form');
-});
+	request({
+		url: url,
+		json: true
+	},function(err, res, body){
+		    if (!err && res.statusCode === 200) {
+	        /*console.log(body.main.temp)*/ 
+	        res.render('data',{
+				temp: body.main.temp,
+				humidity: body.main.humidity,
+				pressure: body.main.pressure,
+				cityname: body.name
+			});
+	    }
 
-app.post('/',function(req, res){
-	let blass = new Class();
-	blass.name = req.body.name;
-	blass.branch = req.body.branch;
-
-	blass.save(function(err){
-		if(err){
-			console.log(err);
-		}
-		else
-		{
-			res.render('form',{
-			name: blass.name,
-			branch: blass.branch
-			});	
-		}
 	});
+	
 });
+
+
 
 app.listen(3000, function(){
 	console.log('server is running');
