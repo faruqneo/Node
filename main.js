0c7e5a6e49de80befe01e0adb679b4a1cc49e195
faruqneo <faruq@marmeto.com>
2019-01-23 14:17:38 +0530

    pagination is not working

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
 
 //Init app
 const app = express();
 
 //Crate connetion
-mongoose.connect('mongodb://localhost/nodekb');
+mongoose.connect('mongodb://localhost/nodekb',{ useNewUrlParser: true } );
 let db = mongoose.connection;
 
 //Check connection
@@ -14,22 +16,29 @@ db.once('open',function(){
 	console.log('Connected to mongodb');
 });
 
-
 //check connection error
 db.on('error',function(){
 	console.log(error);
 });
 
+// Body parser Middelware
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
 //Connected with model
 let Dummy = require('./model/dummy');
 
 //Connected with view page
-app.set('views',path.join(__dirname,'view'));
+app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
+let dumlen = 0;
+let count = 0;
 
-
-app.get('/',function(req, res){
+app.post('/getdata',function(req, res){
+console.log(req.body.offset);
     Dummy.find({},function(err, dummy){
 		if(err)
 		{
@@ -37,12 +46,23 @@ app.get('/',function(req, res){
 		}
 		else
 		{
-			res.render('table',{
-				dummy: dummy
-            });		
-            // res.send(dummy);	
+			res.send(dummy);		
+			dumlen = dummy.length;
 		}
-    });
+    }).sort({id: 1}).limit(10).skip(count);
+});
+
+app.get('/getdata',function(req, res){
+	res.send('Restricted ');
+});
+
+
+app.get('/',function(req, res){
+	res.render('table',{
+		dummy: dumlen,
+		offset: count
+	});		
+	// res.send(dummy);	
 });
 
 
